f4da6f6d701301c864b6a9f4463c68d941d33833
Rituvesh Kumar <rituvesh@gmail.com>
2019-12-10 23:12:11 +0100

    Testing med /person/personopplysninger-api/feature-toggles?feature=pdl-fullmakt url

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -88,7 +88,7 @@ export const sendTilLogin = () => {
   }
 };
 
-export const fetchUnleash = () => hentJson(`${apiUrl}/personopplysninger-api/feature-toggles?feature=pdl-fullmakt`);
+export const fetchUnleash = () => hentJson(`${baseUrl}/person/personopplysninger-api/feature-toggles?feature=pdl-fullmakt`);
 
 export const fetchFodselsnr = () => hentJson(`${apiUrl}/fodselsnr`);
 
